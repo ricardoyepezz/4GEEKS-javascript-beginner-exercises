3a679d5085ff0f9a0300d002d6a17f812cc56dc3
ricardoyepezz <ricardoyepezz@gmail.com>
2022-02-15 15:59:40 +0000

    exercise 23 in process

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,3 +1,18 @@
+function sing () {
+    for (let i=1; i<=12; i++){
+         if (i==12){
+            "whisper words of wisdom"
+        }
+        else if (i== 11) {
+            "there will be an answer,"
+            
+        } else if (i == 12)  {
+            "let it be"        } 
+            else if (i != 12 && i != 11 &&i != 5) {
+            "let it be,"
+        }
+    }
+}
 
 
 //Your code above ^^^
