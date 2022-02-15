d002cd2f4b21cee302eab41d633cb00ae59b768a
ricardoyepezz <ricardoyepezz@gmail.com>
2022-02-15 17:12:16 +0000

    exercise 23 done

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,19 +1,15 @@
-function sing () {
-    for (let i=1; i<=12; i++){
-         if (i==12){
-            "whisper words of wisdom"
-        }
-        else if (i== 11) {
-            "there will be an answer,"
-            
-        } else if (i == 12)  {
-            "let it be"        } 
-            else if (i != 12 && i != 11 &&i != 5) {
-            "let it be,"
-        }
+function sing() {
+  for (let i = 1; i < 13; i++) {
+     if (i == 11) {
+        var text3 = ("there will be an answer");
+    } else if (i == 5) {
+        var text2 = ("whisper words of wisdom");
+    } else if (i != 11 && i != 5) {
+        var text = ("let it be");
     }
 }
-
+return (`${text}, ${text}, ${text}, ${text}, ${text2}, ${text}, ${text}, ${text}, ${text}, ${text}, ${text3}, ${text}`)
+}
 
 //Your code above ^^^
 
